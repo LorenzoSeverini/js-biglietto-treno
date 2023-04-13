252ea02054820683a5d79b2011fbe5d72cd36707
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-04-13 17:10:33 +0200

    feat(js):add js content

--- js/script.js
+++ js/script.js
@@ -1,1 +1,76 @@
-alert("help me please");
+// Js train ticket 
+// event for the button  send
+
+var btnGenerate = document.getElementById("generate");
+btnGenerate.addEventListener("click",
+    
+    function () {
+        var passengerName = document.getElementById("name").value;
+        console.log("passenger name generation:", passengerName);
+
+        var km = document.getElementById("km").value;
+        console.log("km distance generation:", km);
+
+
+        var age = document.getElementById("age").value;
+        console.log("age generation:", age);
+
+        var ticketPrice = Math.floor(km * 0.21);
+        var discount;
+        var discountType;
+        console.log("full price ticket", ticketPrice);
+
+        // age discount
+        if (age == "minor") {
+            discount = ticketPrice * 0.2; 
+            discountType = "Minor price";
+        } else if (age =="over65") {
+            discount = ticketPrice * 0.4;
+            discountType = "Over 65 price";
+        } else {
+            discount = 0;
+            discountType = "Full price";
+        }
+
+        // ticket price
+        var finalPrice = ticketPrice - discount;
+        console.log("Discount price:", discount);
+        console.log("Final ticket price:", finalPrice);
+        console.log("Discount type:", discountType);
+
+        // ticket section
+        document.getElementById("passenger").innerHTML = passengerName;
+        document.getElementById("discount").innerHTML = discountType;
+        document.getElementById("ticket-price").innerHTML = finalPrice + "€";
+
+        var wagonNumber = Math.floor(Math.random() * 9) + 1;
+        console.log("wagon number generator:", wagonNumber);
+        document.getElementById("wagon").innerHTML = wagonNumber;
+
+        // ticket show
+        document.getElementById("ticket-section").className = "bottom show";
+    }   
+)
+
+
+// // event for the button cancel
+
+// var btnGenerate = document.getElementById("cancel");
+// btnGenerate.addEventListener("click",
+//     function () {
+//         // null value
+//         document.getElementById("name").value = "";
+//         document.getElementById("km").value = "";
+//         document.getElementById("age").value = "";
+
+//         document.getElementById("passenger").innerHTML = "";
+//         document.getElementById("discount").innerHTML = "";
+//         document.getElementById("ticket-price").innerHTML = "";
+//         document.getElementById("wagon").innerHTML = "";
+//         document.getElementById("cp-code").innerHTML = "";
+
+//         // ticket not show 
+//         document.getElementById("ticket-section").className = "bottom hidden";
+//     }
+
+// )
